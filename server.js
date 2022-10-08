7603c9847c1ada0ce67b9542f543f36256a93d6f
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-10-08 18:54:15 -0500

    updated and tested data validation

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import {check, validationResult } from 'express-validator';
 
 //initialize express 
 const app = express();
+
 
 //connect to database
 connectDatabase();
@@ -19,10 +21,22 @@ app.get('/', (req, res) =>
 
 //@route POST api/guitars
 //@desc Register Guitar
-app.post('/api/guitars', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
+app.post('/api/guitars', 
+    [
+        check('model', 'Please enter model name').not().isEmpty(),
+        check('finish', 'Please enter finish type').not().isEmpty(),
+        check('brand', 'Please enter guitar brand').not().isEmpty(),
+        check('value', 'Please enter a dollar amount for value').not().isEmpty()
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            res.send(req.body);
+        }        
+    }
+);
 
 //listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
