25b06be4f4a123f8561c3c2f4d3108a5bcb3bb89
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-10-08 21:19:36 -0500

    enabled cors and changed server port localhost to 4000

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+//import { response } from 'express';
 
 class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000')
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      });
+  }
+
   render() {
     return (
     <div className="App">
       <header className="App-header">
         Guitar Collection
       </header>
+      {this.state.data}
     </div>
     );
   }
 }
 
 export default App;
-
-/*<img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>*/
